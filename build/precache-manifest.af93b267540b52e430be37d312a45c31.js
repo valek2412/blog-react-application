e93f3b1fe0448f4dfe5ec9b7191be91811e64dc7
Valek2412 <valoleg99@gmail.com>
2021-02-02 20:24:48 +0200

    without styles and updating post (creating and deleting without redux)

---
+++ build/precache-manifest.af93b267540b52e430be37d312a45c31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8843e65579f46be83689b3c2a9c2f24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be106847ea2f680b0bc",
+    "url": "/static/js/2.e630d0e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e630d0e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c36d18fda777953148",
+    "url": "/static/js/main.311eab7f.chunk.js"
+  },
+  {
+    "revision": "77676e498540a46c6d1a",
+    "url": "/static/js/runtime-main.942762ac.js"
+  }
+]);
