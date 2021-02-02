08e0e777405960aac088871ee228831ba501fed0
Valek2412 <valoleg99@gmail.com>
2021-02-02 20:47:44 +0200

    deploy

--- precache-manifest.1394b87bcc9d1bf31852186e50e53515.js
+++ precache-manifest.1394b87bcc9d1bf31852186e50e53515.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0e8af1cb6ecf8e35bee63b07c42d72de",
+    "revision": "407f927a110ad764959cd15e7fcfde20",
     "url": "/index.html"
   },
   {
-    "revision": "ead42ed185fde03be828",
-    "url": "/static/js/2.ee05ea33.chunk.js"
+    "revision": "be285253c94d65eab16b",
+    "url": "/static/js/2.906925f8.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.ee05ea33.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.906925f8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5498884520cb08816941",
-    "url": "/static/js/main.dabcb113.chunk.js"
+    "revision": "6bb72df3bddfbc5a2ac3",
+    "url": "/static/js/main.44f7f4fe.chunk.js"
   },
   {
     "revision": "77676e498540a46c6d1a",
