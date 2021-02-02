3451178954a19749a6a4f5df038c2aa1f45c804c
Valek2412 <valoleg99@gmail.com>
2021-02-02 20:31:31 +0200

    without styles and updating post (creating and deleting without redux)

--- build/precache-manifest.4c478d78e1d9986c5719b7c24379ff54.js
+++ build/precache-manifest.4c478d78e1d9986c5719b7c24379ff54.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b8843e65579f46be83689b3c2a9c2f24",
+    "revision": "0e8af1cb6ecf8e35bee63b07c42d72de",
     "url": "/index.html"
   },
   {
-    "revision": "2be106847ea2f680b0bc",
-    "url": "/static/js/2.e630d0e6.chunk.js"
+    "revision": "ead42ed185fde03be828",
+    "url": "/static/js/2.ee05ea33.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.e630d0e6.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.ee05ea33.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "69c36d18fda777953148",
-    "url": "/static/js/main.311eab7f.chunk.js"
+    "revision": "5498884520cb08816941",
+    "url": "/static/js/main.dabcb113.chunk.js"
   },
   {
     "revision": "77676e498540a46c6d1a",
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 
 import { Theme as ThemeProvider } from './styles/theme';
